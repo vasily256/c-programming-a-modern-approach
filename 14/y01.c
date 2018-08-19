6e11de21ed31e463d0ae7ad9aed7ba67d012596b
nanotech <vasily@markov.biz>
2018-08-19 17:19:45 +0300

    Ex. 14.2 started

--- 14/y01.c
+++ 14/y01.c
@@ -1,25 +1,27 @@
 #include <stdio.h> 
 
-#define INT
-
-#if deined(INT)
-#define TYPE int 
-#define T d 
-
-#elif deined(DOUBLE)
-#define TYPE double
-#define T lf 
-#endif
-
 #define CUBE(x)           ((x) * (x) * (x))         // (a)
 #define REMINDER(x)       ((x) % 4)                 // (b)
 #define TEST_PRODUCT(x,y) ((x) * (y) < 100 ? 1 : 0) // (c)
 
 int main(void)
 {
-    TYPE x, y;
+    int x, y;
+
+    // (a)
     printf("Enter x and y values: ");
-    scanf("%T %T", &x, &y);
+    scanf("%d %d", &x, &y);
+
+    // (b)
+    printf("%d ^ 3 = %d\n", x, CUBE(x));
+    printf("%d %% 4 = %d\n", x, REMINDER(x)); 
+
+    // (c)
+    printf("%d * %d", x, y); 
+    if(TEST_PRODUCT(x, y))
+        printf(" < 100\n");
+    else 
+        printf(" > 100\n");
 
     return 0;
 }
---
+++ 14/y02.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+#define NELEMS(a) (sizeof(a) / sizeof(a[0])
+
+int main(void)
+{
+    int n;
+
+    printf("Enter number of array elements: ");
+    scanf("%d", &a);
+    printf("Number of array elements: %d")
+
+    int a[n];
+    return 0;
+}
