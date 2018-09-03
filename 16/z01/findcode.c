7aa2ee6f08151a9acda90e78a4fb53be5453c5eb
nanotech <vasily@markov.biz>
2018-09-03 22:05:46 +0300

    Completed proj.1 of ch.16

--- 16/z01/findcode.c
+++ 16/z01/findcode.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
-#include "codes.h"
-
-#if 0
-#include <stdbool.h>
-#include <ctype.h>
-#include <string.h>
 #include <stdlib.h>
-#endif
+#include "codes.h"
 
 int find_code(int code, int index);
 
 int main(void)
 {
     int code, index = 0;
-
+ 
     printf("Enter country code (0 - exit): ");
-    scanf("%d", &code);
-
-    while (code != 0 && index < num_codes) {
-        index = find_code(code, index);
-        puts(country_codes[index].country);
-    }
-
+    scanf("%d", &code); 
+    if (code == 0)
+        return 0; 
+    while ((index = find_code(code, index)) < num_codes)
+        puts(country_codes[index++].country); 
     return 0;
+}
+
+int find_code(int code, int index)
+{
+    int initial_index = index;
+
+    for (; index < num_codes; index++)
+        if (country_codes[index].code == code)
+            return index;
+
+    if (initial_index == 0) {
+        printf("Code not found\n");
+        exit(EXIT_SUCCESS);
+    }
 }
