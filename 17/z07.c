57967a3beed58cd2a74364d25c4b86554bc20894
nanotech <vasily@markov.biz>
2018-09-14 18:23:44 +0300

    Completed proj.7 of ch.17

--- 17/z07.c
+++ 17/z07.c
@@ -19,22 +19,33 @@
 #define MSG_LEN 60      /* max length of reminder message */
 
 struct vstring {
+  int max_len;
   int len;
   char chars[];
 };
 
-void Vread_line(struct vstring *vstr, int n);
+void Vread_line(struct vstring *vstr);
 int Vstrcmp(const struct vstring *vstr1, const struct vstring *vstr2);
-void Vstrcpy(struct vstring *vstr1, const struct vstring *vstr2);
+struct vstring *Vstrcpy(struct vstring *vstr1, const struct vstring *vstr2);
+struct vstring *Vstrcat(struct vstring *vstr1, const struct vstring *vstr2);
+void Vputs(struct vstring *vstr);
 
 int main(void)
 {
+  int day, i, j, num_remind = 0;
   struct vstring *reminders[MAX_REMIND]; 
   struct vstring *day_str = malloc(sizeof(struct vstring) + 3);
-  day_str->len = 3;
+  if (day_str == NULL) {
+    printf("malloc: allocation failed\n");
+    return 0;
+  }
+  day_str->max_len = 3;
   struct vstring *msg_str = malloc(sizeof(struct vstring) + MSG_LEN);
-  msg_str->len = MSG_LEN;
-  int day, i, j, num_remind = 0;
+  if (msg_str == NULL) {
+    printf("malloc: allocation failed\n");
+    return 0;
+  }
+  msg_str->max_len = MSG_LEN;
 
   for (;;) {
     if (num_remind == MAX_REMIND) {
@@ -47,8 +58,8 @@ int main(void)
     if (day == 0)
       break;
     sprintf(day_str->chars, "%2d", day);
-    day_str->len = 2;
-    Vread_line(msg_str, MSG_LEN);
+    day_str->len = strlen(day_str->chars);
+    Vread_line(msg_str);
 
     for (i = 0; i < num_remind; i++)
       if (Vstrcmp(reminders[i], day_str) > 0)
@@ -61,6 +72,7 @@ int main(void)
       printf("-- No space left --\n");
       break;
     }
+    reminders[i]->max_len = day_str->len + msg_str->len;
 
     Vstrcpy(reminders[i], day_str);
     Vstrcat(reminders[i], msg_str);
@@ -69,18 +81,20 @@ int main(void)
   }
 
   printf("\nDay Reminder\n");
-  for (i = 0; i < num_remind; i++)
-    printf(" %s\n", reminders[i]);
+  for (i = 0; i < num_remind; i++) {
+    putchar(' ');
+    Vputs(reminders[i]);
+  }
 
   return 0;
 }
 
-void Vread_line(struct vstring *vstr, int n)
+void Vread_line(struct vstring *vstr)
 {
   int ch, i = 0;
 
   while ((ch = getchar()) != '\n')
-    if (i < n)
+    if (i < vstr->max_len)
       vstr->chars[i++] = ch;
   vstr->len = i;
 }
@@ -90,10 +104,11 @@ int Vstrcmp(const struct vstring *vstr1, const struct vstring *vstr2)
   int i = 0;
   bool equal = true;
 
-  for (;  equal == true && i < vstr1->len && i < vstr2->len; i++)
+  for (;  equal == true && i < vstr1->len && i < vstr2->len; i++) {
     if (vstr1->chars[i] != vstr2->chars[i]) {
       equal = false;
     }
+  }
   if (equal == true)
     if (vstr1->len > vstr2->len)
       return 1;
@@ -102,17 +117,36 @@ int Vstrcmp(const struct vstring *vstr1, const struct vstring *vstr2)
     else
       return -1;
   else
-    if (vstr1->chars[i] > vstr2->chars[i])
+    if (vstr1->chars[i-1] > vstr2->chars[i-1])
       return 1;
     else
       return -1;
 }
 
-Vstrcpy(struct vstring *vstr1, const struct vstring *vstr2)
+struct vstring *Vstrcpy(struct vstring *vstr1, const struct vstring *vstr2)
 {
   int i;
 
-  for(i = 0; i < vstr1->len; i++)
+  for(i = 0; i < vstr1->max_len && i < vstr2->len; i++)
     vstr1->chars[i] = vstr2->chars[i];
   vstr1->len = i;
+
+  return vstr1;
+}
+
+struct vstring *Vstrcat(struct vstring *vstr1, const struct vstring *vstr2)
+{
+  int i, j;
+
+  for(i = vstr1->len, j = 0; i < vstr1->max_len && j < vstr2->len; i++, j++)
+    vstr1->chars[i] = vstr2->chars[j];
+  vstr1->len = i;
+
+  return vstr1;
+}
+void Vputs(struct vstring *vstr)
+{
+  for (int i = 0; i < vstr->len; i++)
+    putchar(vstr->chars[i]);
+  putchar('\n');
 }
